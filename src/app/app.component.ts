ecd37b94be8be55c1930d5c4c89922ba9aa5b7c2
RuslanYaremko <99643936+RuslanYaremko@users.noreply.github.com>
2024-11-12 13:17:48 +0200

    joke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +7,23 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'jokes';
+  joke: string = 'Натисніть кнопку, щоб отримати жарт!';
+
+  constructor(private http: HttpClient) {}
+
+  getJoke() {
+    this.http.get<any>('https://webart.work/api/kpnu/joke').subscribe(
+      (data) => {
+        this.joke = data.joke || 'Не вдалося отримати жарт.';
+      },
+      (error) => {
+        console.error('Error fetching joke:', error);
+        this.joke = 'Помилка при завантаженні жарту.';
+      }
+    );
+  }
 }
