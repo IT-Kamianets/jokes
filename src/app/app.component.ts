38b523984c4f6b5eacd7c3a335adc2ce2bf90a9f
Dmytro-Andronik <99643788+Dmytro-Andronik@users.noreply.github.com>
2024-11-19 12:49:11 +0200

    Joke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppModule { }
+export class AppComponent {
+  title = 'Жарти'; // Заголовок сторінки
+  joke: string = ''; // Місце для жарту
+
+  // URL для отримання жартів
+  private jokeApiUrl = 'https://webart.work/api/kpnu/joke';
+
+  constructor(private http: HttpClient) {}
+
+  // Метод для отримання жарту
+  getJoke(): void {
+    this.http.get<any>(this.jokeApiUrl).subscribe({
+      next: (response) => {
+        this.joke = response.joke || 'Жарт не вдалося завантажити.';
+      },
+      error: (err) => {
+        console.error('Помилка при отриманні жарту:', err);
+        this.joke = 'Сталася помилка при завантаженні жарту.';
+      }
+    });
+  }
+}
