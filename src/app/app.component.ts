e05b1fc65987d14518c7a5b4d3c1c9d230024c0d
Dmytro-Andronik <99643788+Dmytro-Andronik@users.noreply.github.com>
2024-11-19 13:02:30 +0200

    Joke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-joke',
-  templateUrl: './joke.component.html',
-  styleUrls: ['./joke.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class JokeComponent {
-  title = 'Жарти з API';
-  joke = 'Натисніть кнопку, щоб отримати жарт!';
-
-  constructor(private http: HttpClient) {}
-
-  getJoke() {
-    const apiUrl = 'https://webart.work/api/kpnu/joke';
-    this.http.get<{ joke: string }>(apiUrl).subscribe({
-      next: (response) => {
-        this.joke = response.joke;
-      },
-      error: () => {
-        this.joke = 'Не вдалося завантажити жарт. Спробуйте ще раз!';
-      }
-    });
-  }
+export class AppComponent {
+  title = 'Жарти';
 }
